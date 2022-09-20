ba67365e1cff8a02f9fb05e38e9e8f8cd9ce8319
JmuXx <jimsoulier8@gmail.com>
2022-09-20 16:22:35 +0200

    PART 3 - JAVA

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,9 @@ public class Launcher {
     static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Bienvenue, que vouliez-vous me dire ? :");
-        var a = s.nextLine();
-        if(!"quit".equals(a)){
-            System.out.println("Unknown command");
+        String input;
+        while(!"quit".equals(input = s.nextLine())) {
+                System.out.println("Unknown command");
         }
     }
 }
